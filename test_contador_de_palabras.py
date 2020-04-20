5e5ffb4cfab41de51518294806b141e8120f2c65
asg-md <usuario@curso.es>
2020-04-20 20:23:43 +0200

    Versión 1 de estimador de PI

--- test_contador_de_palabras.py
+++ test_contador_de_palabras.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from contador_de_palabras import cargar_texto, contador_de_palabras
+from contador_de_palabras import cargar_texto, contador_de_palabras, limpiar_acentos
 
 
 class Test(TestCase):
@@ -8,3 +8,9 @@ class Test(TestCase):
         texto = cargar_texto('texto_prueba.txt')
         n_palabras = contador_de_palabras(texto)
         self.assertEquals(n_palabras,15)
+
+
+class Test(TestCase):
+    def test_limpiar_acentos(self):
+        texto = 'Hóla añigo'
+        self.assertEquals(limpiar_acentos(texto),'Hola anigo')
