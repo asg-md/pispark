dd8332cdd70bd62bb72029e70047478137796e5a
asg-md <48068214+asg-md@users.noreply.github.com>
2020-04-22 15:04:01 +0200

    Add files via upload

--- contador_de_palabras.py
+++ contador_de_palabras.py
@@ -1,12 +1,17 @@
 import re
 import unidecode
+import pandas as pd
+
+# def cargar_texto(filename):
+#     with open(filename, encoding="utf8") as f:
+#         lines = f.readlines()
+#         lines = [x.strip() for x in lines]
+#     f.close()
+#     return lines
 
 def cargar_texto(filename):
-    with open(filename, encoding="utf8") as f:
-        lines = f.readlines()
-        lines = [x.strip() for x in lines]
-    f.close()
-    return lines
+    with open(filename, mode = 'r', encoding="utf8") as fichero:
+        return fichero.read()
 
 
 def partidor_palabras(texto):
@@ -38,6 +43,7 @@ def partidor_palabras(texto):
         "[\"]|" +
         "[”]" +
         "[”]" +
+        "[\n]" +
         "[0-9]+|" +
         "\s", texto)
     result = [individual_word for individual_word in result if len(individual_word) > 0]
@@ -47,18 +53,28 @@ def partidor_palabras(texto):
 def limpiar_acentos(texto):
     return unidecode.unidecode(texto)
 
+PALABRAS_stopwordsspanish = None
+def cargar_stopwordsspanish():
+    global PALABRAS_stopwordsspanish
+    if PALABRAS_stopwordsspanish == None:
+        filename = "stopwordsspanish.txt"
+        stopwords_l = cargar_texto(filename)
+        PALABRAS_stopwordsspanish = partidor_palabras(limpiar_acentos(stopwords_l).upper())
+    return PALABRAS_stopwordsspanish
 
-def contador_de_palabras(lineas_texto):
-    lista_txt_all = []
-    for i in lineas_texto:
-        lista_txt_all = lista_txt_all + partidor_palabras(limpiar_acentos(i))
-    return len(lista_txt_all)
+def contar_words(texto_limpio_partido):
+    stopwords_limpio_partido = cargar_stopwordsspanish()
+    cuenta_df = pd.DataFrame({'Palabras': list(filter(lambda elem: elem not in stopwords_limpio_partido, texto_limpio_partido)), 'Repeticiones':  1})
+    return cuenta_df.groupby(by='Palabras').agg({'Repeticiones': 'count'}).reset_index().sort_values(by=['Repeticiones'], ascending=False)
 
-
-
-# filename = "el_quijote.txt"
-filename = "texto_prueba.txt"
+filename = "el_quijote.txt"
+# filename = "texto_prueba.txt"
 
 lines = cargar_texto(filename)
-n_palabras = contador_de_palabras(lines)
-print(n_palabras)
+texto_limpio_partido = partidor_palabras(limpiar_acentos(lines).upper())
+contar_words_df = contar_words(texto_limpio_partido)
+
+# contar_words_df
+print(contar_words_df)
+
+
--- test_contador_de_palabras.py
+++ test_contador_de_palabras.py
@@ -1,12 +1,12 @@
 from unittest import TestCase
 
-from contador_de_palabras import cargar_texto, contador_de_palabras, limpiar_acentos
+from contador_de_palabras import cargar_texto, limpiar_acentos, partidor_palabras
 
 
 class Test(TestCase):
     def test_partidor_palabras(self):
         texto = cargar_texto('texto_prueba.txt')
-        n_palabras = contador_de_palabras(texto)
+        n_palabras = len(partidor_palabras(limpiar_acentos(texto).upper()))
         self.assertEquals(n_palabras,15)
 
 
